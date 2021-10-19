381b5aac20d6845f66c2b879aaec3d607767b3a8
skunwar1508 <skunwar1508@gmail.com>
2021-10-19 11:27:42 +0530

    address

--- components/addressCard.js
+++ components/addressCard.js
@@ -18,8 +18,8 @@ export default function AddressCards(props){
     function addressEdit(id){
         console.log(id, 'edit');
     }
-    function addressDelete(id){
-        console.log(id, 'Delete');
+    function addressDefault(id){
+        console.log(id, 'address Default');
     }
     return(
         <>
@@ -30,9 +30,9 @@ export default function AddressCards(props){
                     <div className="my_address border rounded-3 p-3">
                         <h6>Home</h6>
                         <p> {data.address} </p>
-                        <ul className="d-flex flex-wrap justify-content-between align-items-center ">
-                            <li> <a onClick={()=>addressEdit(data._id)}> Edit </a> </li>
-                            <li> <a onClick={()=>addressDelete(data._id)}> Delete </a> </li>
+                        <ul className="d-flex flex-wrap justify-content-between align-items-center">
+                            <li><a onClick={()=>addressEdit(data._id)}> Edit </a> </li>
+                            <li><a onClick={()=>addressDefault(data._id)}> Set As Default </a> </li>
                         </ul>
                     </div>
                 </div>
